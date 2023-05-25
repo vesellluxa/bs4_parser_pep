7617c026baf5e40f1f0b1d8e3f56bbe6bd67ad89
Алексей Шевич <lehashevich@gmail.com>
2023-05-25 19:39:48 +0300

    lets roll 4

--- src/main.py
+++ src/main.py
@@ -30,6 +30,7 @@ PARSER_START_LOGGING_INFO = 'Парсер запущен!'
 PARSER_START_ARGS_LOGGING_INFO = 'Аргументы при запуске: {args}'
 PARSER_COMPLETED_WORK = 'Парсер завершил работу!'
 FAILED_CREATE_SOUP_INFO = 'Не удалось создать SOUP! {url}, {error}'
+PARSER_WORK_ERROR = 'Во время выполнения работы парсера произошла ошибка! {error}'
 
 
 def whats_new(session):
@@ -232,7 +233,11 @@ def main():
         if results is not None:
             control_output(results, args)
     except Exception as error:
-        logging.info(error)
+        logging.info(
+            PARSER_WORK_ERROR.format(
+                error=error
+            )
+        )
     logging.info(PARSER_COMPLETED_WORK)
 
 
